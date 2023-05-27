b1cc0b1f27638f51379d6d8cdba5dac3de04c046
imanewazany <imane.elouazzany@gmail.com>
2023-05-27 15:15:47 -0700

    task 0

---
+++ test/0_main.c
@@ -0,0 +1,17 @@
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	printf("_printf => %d\nprintf => %d\n", len, len2);
+	return (0);
+}
