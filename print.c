01dd51234c6d821bbd2d32658c2994ce54397249
imanewazany <imane.elouazzany@gmail.com>
2023-05-27 12:19:09 -0700

    commit

---
+++ print.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ * @Return: the number of characters printed
+ *	(excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	int numc, i;
+	va_list L;
+
+	va_start(L, format);
+	i = 0
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			switch (format[i + 1])
+			{
+				case 's':
+					printf("%s", va_arg(L, char *));
+					break;
+				case 'c':
+					printf("%c", va_arg(L, int));
+					break;
+				case 'i':
+					printf("%i", va_arg(L, int));
+					break;
+				case 'd':
+					printf("%d", va_arg(L, int));
+					break;
+			}
+		}
+		else
+		{
+			printf("%s", format[i]);
+			numc++;
+		}
+		i++;
+	}
+	va_end(L);
+	return (numc);
+}
