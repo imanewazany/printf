4b81eaaba93560f25d07832ea4c2c4cb08119954
imanewazany <imane.elouazzany@gmail.com>
2023-05-28 12:35:32 -0700

    task 0

--- test/0-main.c
+++ test/0-main.c
@@ -17,7 +17,12 @@ int main(void)
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	printf("_printf => %d\nprintf => %d\n", len, len2);
-	_printf("_printf =>%c\n", c);
-	_printf("_printf =>%s\n", s);
+	_printf("_printf => %c\n", c);
+	_printf("_printf => %s\n", s);
+	_printf("_printf => %c, %%, %s\n", c, s);
+	_printf("%%");
+	_printf("\n");
+	_printf("%c");
+	_printf("\n");
 	return (0);
 }
