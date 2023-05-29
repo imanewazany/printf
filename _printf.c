b0b2748146192d6fedc2a2cf83f6d985dbcc206b
Ayshataje <imagbyai@gmail.com>
2023-05-29 16:04:22 -0700

    t

--- _printf.c
+++ _printf.c
@@ -41,7 +41,7 @@ int _printf(const char *format, ...)
 	va_start(L, format);
 	i = 0;
 	numc = 0;
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
        		 return (-1); 
