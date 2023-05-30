f948b0b9d9aa90709f6dbd77345297cff925b679
imanewazany <imane.elouazzany@gmail.com>
2023-05-30 11:11:03 -0700

    betty style

--- _printf.c
+++ _printf.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 int print_format(va_list L, char f);
+
 /**
- * printf_format - print argument according a specific formating 
+ * print_format - print argument according a specific formating
  * @L: va_list
- * @f: caracter to handel 
+ * @f: caracter to handel
  * Return: number of printed caracter
- * */
+ **/
 int print_format(va_list L, char f)
 {
 	int i;
@@ -35,13 +37,14 @@ int _printf(const char *format, ...)
 {
 	int numc, i, s;
 	va_list L;
+
 	va_start(L, format);
 	i = 0;
 	numc = 0;
 	if (format == NULL || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-       		 return (-1); 
+		return (-1);
 	while (format && format[i] != '\0')
 	{
 		if (format[i] == '%')
@@ -52,7 +55,7 @@ int _printf(const char *format, ...)
 				numc++;
 				i++;
 			}
-		      	else
+			else
 			{
 				s = print_format(L, format[i + 1]);
 				if (s == 0)
@@ -70,10 +73,10 @@ int _printf(const char *format, ...)
 		else
 		{
 			_putchar(format[i]);
-			numc+i+;
+			numc++;
 		}
 		i++;
 	}
 	va_end(L);
-		return (numc);
-	}
+	return (numc);
+}
